e425159d86fda7a03756ab5feea00f93eb5a945a
NiltonMeira <niltonprudys@gmail.com>
2024-09-12 16:35:38 -0300

    feat: handleError

---
+++ src/middlewares/handleError.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import AppError from "../appError";
+
+export const handleError = async (error: Error, req: Request, res: Response, next: NextFunction) => {
+    if(error instanceof AppError){
+    return res.status(error.statusCode).json({message: error.message})
+        
+    }
+
+    console.error(error)
+    return res.status(500).json({message: "Internal server error"})
+    
+}
