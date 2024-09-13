db16ba63e36adcf4042b532d2bdb658dbc4241f4
NiltonMeira <niltonprudys@gmail.com>
2024-09-13 16:48:06 -0300

    feat: hashing password

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,3 +1,4 @@
+import  bcrypt  from 'bcryptjs';
 import AppError from "../appError"
 import { User } from "../models/userModel"
 import { tUserCreation } from "../types/userTypes"
@@ -5,6 +6,8 @@ import { tUserCreation } from "../types/userTypes"
 export const creationUserService = async(payload: tUserCreation ) => {
     if(!await validateEmail(payload.email).valueOf()) throw new AppError("Email ja cadastrado", 404) 
     if(!(await validateEdv(payload.edv)).valueOf()) throw new AppError("EDV ja cadastrado", 404)
+
+    payload.password = await hashPassword(payload.password);
 
     const newUser = new User(payload)
     newUser.role = 1
@@ -25,5 +28,9 @@ export const validateEdv = async (edv: string) => {
     )
 
     return users.length == 0
-    
+}
+
+export const hashPassword = async(password: string) => {
+    const salt = await bcrypt.genSalt(12)
+    return bcrypt.hash(password, salt)
 }
