21784ce148b536b4a1ac686d528c132cbdc0fe2d
Nilton <niltonprudys@gmail.com>
2024-09-16 19:52:54 -0300

    feat: Product and Family Creation and Update types

--- src/types/familyType.ts
+++ src/types/familyType.ts
@@ -4,3 +4,6 @@ export type TFamily = {
     productid: string,
     description: string
 }
+
+export type TFamilyCreation = Omit<TFamily, "id">
+export type TFamilyUpdate = Partial<TFamily>
--- src/types/productType.ts
+++ src/types/productType.ts
@@ -1,5 +1,9 @@
+import { tUserCreation, TUserUpdate } from './userTypes';
 export type TProduct = {
     id: string,
     name: string,
     description: string
 }
+
+export type TProductCreation = Omit<TProduct, "id">
+export type TProductUpdate = Partial<tUserCreation>
