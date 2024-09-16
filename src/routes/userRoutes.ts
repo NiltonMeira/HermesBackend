adc842743d817ddde445909792eb78ad322941e0
NiltonMeira <niltonprudys@gmail.com>
2024-09-16 10:47:25 -0300

    feat: user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-import { creationUserController} from '../controllers/userControllers';
+import { creationUserController, deleteUserController, getUsersController, patchUserController} from '../controllers/userControllers';
+import { getUserByIDService } from '../services/userServices';
 
 const userRouter = express.Router()
 
 userRouter.post('', creationUserController)
-userRouter.get('', getAllUsersController)
+userRouter.get('', getUsersController)
+userRouter.get("/:id", getUserByIDService)
+userRouter.delete("/:id", deleteUserController)
+userRouter.patch("/:id", patchUserController)
 
 export default userRouter
