ef883a96c428cce415ac2bef43575a6ed44579dd
Nilton <niltonprudys@gmail.com>
2024-09-18 18:51:44 -0300

    feat: components services

--- src/controllers/componentController.ts
+++ src/controllers/componentController.ts
@@ -1,7 +1,6 @@
-import { getComponentByIdService, getComponentByNameService } from './../services/componentService';
+import { deleteComponentService, getComponentByIdService, getComponentByNameService, getComponentsByPartNumberService, patchComponentService } from './../services/componentService';
 import { Request, Response } from "express";
 import { creationComponentService, getAllComponentsService } from "../services/componentService";
-import AppError from '../appError';
 
 export const creationComponentController = async (req: Request, res: Response) => {
     const service = await creationComponentService(req.body)
@@ -14,10 +13,26 @@ export const getComponentById = async(req: Request, res: Response) => {
 }
 
 export const getComponentsController = async (req: Request, res: Response) => {
-    const query = await req.query.getComponentByNameService
+    const query = await req.query.componentName
 
     const service = query ?
     getComponentByNameService(query as string) :
     getAllComponentsService()
 
+    res.status(200).json(service)
+}
+
+export const getPartNumberController = async (req: Request, res: Response) => {
+    const service = await getComponentsByPartNumberService(req.params.id)
+    res.status(200).json(service)
+}
+
+export const deleteComponentController = async (req: Request, res: Response) => {
+    const service = await deleteComponentService(req.params.id)
+    res.status(204).json(service)
+}
+
+export const patchComponentController = async (req: Request, res: Response) => {
+    const service = await patchComponentService(req.body, req.params.id)
+    res.status(200).json(service)
 }
