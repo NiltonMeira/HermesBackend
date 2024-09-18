1382648362298c0668749cf3168537f9b9985fa2
NiltonMeira <niltonprudys@gmail.com>
2024-09-18 12:56:39 -0300

    feat: component servivces

--- src/services/componentService.ts
+++ src/services/componentService.ts
@@ -1,22 +1,61 @@
+import AppError from "../appError"
 import { Component } from "../models/componentModel"
 import { TComponentCreation } from "../types/componentType"
 
 export const creationComponentService = async (payload: TComponentCreation) => {
-    const newComponent =  new Component(payload)
+    const newComponent = new Component(payload)
     return await newComponent.save()
 }
 
 export const getComponentByIdService = async (id: string) => {
     const component = await Component.findById(id)
+
+    if (!component) throw new AppError("Component not found", 404)
     return component
 }
 
 export const getAllComponents = async () => {
-    return await Component.find()
+    const components = await Component.find()
+
+    if (!components) throw new AppError("Component not found", 404)
+
+    return components
 }
 
-export const getComponentByName = async (name: string) => {
-const components = await Component.find(
-    
-) 
+export const getComponentByNameService = async (name: string) => {
+    const components = await Component.find(
+        { "name": { "$regex": name, "$options": "i" } }
+    )
+
+    if (!components) throw new AppError("Component not found", 404)
+
+    return components
 }  
+
+export const getComponentsByPartNumberService = async (partNUmber: string) => {
+    const components = await Component.find(
+        { "name": { "$regex": partNUmber, "$options": "i" } }
+
+    )
+
+    if (!components) throw new AppError("Component not found", 404)
+
+    return components
+
+}
+
+export const deleteComponentService = async (id: string) => {
+    const component = Component.findById(id)
+
+    if(!component) throw new AppError("Component not found", 404)
+
+    await component.deleteOne()
+
+}
+
+
+export const patchComponentService = async () => {
+    const component = Component.find(
+        
+    )
+}
