ebf9d338c090453776db2d509b97e0df03dc2d07
NiltonMeira <niltonprudys@gmail.com>
2024-09-18 12:14:31 -0300

    feat: part of the service component

---
+++ src/services/componentService.ts
@@ -0,0 +1,22 @@
+import { Component } from "../models/componentModel"
+import { TComponentCreation } from "../types/componentType"
+
+export const creationComponentService = async (payload: TComponentCreation) => {
+    const newComponent =  new Component(payload)
+    return await newComponent.save()
+}
+
+export const getComponentByIdService = async (id: string) => {
+    const component = await Component.findById(id)
+    return component
+}
+
+export const getAllComponents = async () => {
+    return await Component.find()
+}
+
+export const getComponentByName = async (name: string) => {
+const components = await Component.find(
+    
+) 
+}  
