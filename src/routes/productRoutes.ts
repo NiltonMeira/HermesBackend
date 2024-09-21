f7d70794f5bece6cc826ab5b34e5aa7fa1b80463
Nilton <niltonprudys@gmail.com>
2024-09-21 19:34:28 -0300

    feat: product auth

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,12 +1,16 @@
 import  express  from 'express';
 import { creationProductController, deleteProductsController, getProductsByIdController, getProductsController, patchProductController } from '../controllers/productController';
+import { validateToken } from '../middlewares/validateToken';
+import { validateAdm } from '../middlewares/validateAdm';
+import { validatePlanner } from '../middlewares/validatePlanner';
+import { validateOperator } from '../middlewares/validateOperator';
 
 const productRouter = express.Router()
 
-productRouter.post('', creationProductController)
-productRouter.get('', getProductsController)
-productRouter.get('/:id', getProductsByIdController)
-productRouter.delete('/:id', deleteProductsController)
-productRouter.patch('/:id', patchProductController)
+productRouter.post('',validateToken, validatePlanner, creationProductController)
+productRouter.get('',validateToken, validateOperator, getProductsController)
+productRouter.get('/:id',validateToken, validateOperator, getProductsByIdController)
+productRouter.delete('/:id',validateToken, validateAdm, deleteProductsController)
+productRouter.patch('/:id',validateToken, validateAdm, patchProductController)
 
 export default productRouter
