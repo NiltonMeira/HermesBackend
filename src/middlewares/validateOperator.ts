a85d1ae0f48950bb237c30a10ef5f768ae82abaa
Nilton <niltonprudys@gmail.com>
2024-09-26 00:43:59 -0300

    feat: verification midleware

--- src/middlewares/validateOperator.ts
+++ src/middlewares/validateOperator.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import AppError from "../appError";
 
 export const validateOperator = async (req: Request, res: Response, next: NextFunction) => {
-    if( Number(res.locals.role) <= 3)
+    if( Number(res.locals.role) > 3)
         throw new AppError("You don't have permission for that", 403)
 
     console.log("User authorized");
