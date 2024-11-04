ca0af6d11c8dbfc2285a35132cb899dd63de62c4
Nilton Meira <niltonprudys@gmail.com>
2024-11-04 10:14:46 -0300

    feat: deploy config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import mongoose from "mongoose"
+import 'dotenv/config'
 import app from "./app"
 
+
 const startServer = async () => {
-    await mongoose.connect("mongodb://localhost:27017/HermesDB")
+    await mongoose.connect(process.env.DB_URL!)
     app.listen(1960, () => console.log("Listening to port 1960")
     )
 }
 
 startServer()
+
+
